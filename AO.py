38a3f084989d2e1bd4a5b04730ec08fd7fe0515a
smritiVishwa <rktsmriti2001@gmail.com>
2022-11-01 13:19:26 +0530

    first commit

---
+++ AO.py
@@ -0,0 +1,10 @@
+a=float(input("Enter the first no :"))
+b=float(input("Enter the second no :"))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print("sum is :",c)
+print("sub is :",d)
+print("multi is :",e)
+print("div is :",f)
---
+++ fac using for.py
@@ -0,0 +1,5 @@
+a=int(input("Enter The number :"))
+c=1
+for i in range(1,a-1):
+    c=c*i
+print("factorial of",a,"is",c)
